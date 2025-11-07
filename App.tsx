a7aee6fb011b7b68be36db69134d26ea3b34da31
JoseAntonio1DAM <usr4034@salesianos-lacuesta.net>
2025-11-07 19:39:22 +0000

    hecha la 1era pantalla y el ej2

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import CarList from './components/CarList';
+import { Car } from './types/Car';
 
-export default function App() {
+const App: React.FC = () => {
+  const [cars, setCars] = useState<Car[]>([]); // Lista vacía por defecto
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CarList cars={cars} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffffff', // Fondo oscuro como en tu imagen
   },
 });
+
+export default App;
---
+++ types/Car.ts
@@ -0,0 +1,7 @@
+export interface Car {
+  id: string;
+  nombre: string;
+  categoria: string;
+  precio: number;
+  marcado: boolean;
+}
